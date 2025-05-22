9d85b9176361f54f6e943417a07e8fcf6648516a
Jamie Wright <jamie@brilliantfantastic.com>
2025-05-22 00:59:46 -0400

    Add youtube shortcode

--- config/config.exs
+++ config/config.exs
@@ -3,7 +3,18 @@ import Config
 config :griffin_ssg,
   input: "src",
   output: "_site",
-  passthrough_copies: ["assets/css/*.css", "assets/images/**/*.{jpg,png,svg}", "assets/js/*.js"]
+  passthrough_copies: ["assets/css/*.css", "assets/images/**/*.{jpg,png,svg}", "assets/js/*.js"],
+  shortcodes: %{
+    youtube: fn slug ->
+      """
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/#{slug}"
+              title="YouTube player" frameborder="0" allow="accelerometer;
+              autoplay; clipboard-write; encrypted-media; gyroscope;
+              picture-in-picture; web-share" allowfullscreen>
+      </iframe>
+      """
+    end
+  }
 
 config :tailwind,
   version: "4.0.0",
